364b33bed1479129221dd72622bea19ad5fcfb99
joseph <josephrios56@gmail.com>
2023-04-21 11:12:57 -0500

    feat: updated demo app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,10 @@
 		disconnectWagmi,
 		signerAddress,
 		connection,
+		loading,
 		chainId,
-		web3Modal
+		web3Modal,
+		wagmiLoaded
 	} from '$lib/stores/wagmi';
 	import { onMount } from 'svelte';
 
@@ -21,17 +23,23 @@
 	);
 </script>
 
-<h1>Svelte Wagmi</h1>
-<p>Simple Wagmi wrapper to take use of sveltekit stores</p>
-{#if $connected}
-	<p>{$signerAddress}</p>
-	<p>chain ID: {$chainId}</p>
-	<button on:click={disconnectWagmi}>disconnect</button>
-{:else}
-	<p>not connected</p>
-	<p>Connect With walletconnect</p>
-	<button on:click={async () => $web3Modal.openModal()}>connect</button>
+{#if wagmiLoaded}
+	<h1>Svelte Wagmi</h1>
+	<p>Simple Wagmi wrapper to take use of sveltekit stores</p>
+	{#if $loading && !$connected}
+		<h1>Loading Data...</h1>
+	{:else if $connected && !$loading}
+		<p>{$signerAddress}</p>
+		<p>chain ID: {$chainId}</p>
+		<button on:click={disconnectWagmi}>disconnect</button>
+	{:else}
+		<p>not connected</p>
+		<p>Connect With walletconnect</p>
+		<button on:click={async () => $web3Modal.openModal()}>connect</button>
 
-	<p>Connect With InjectedConnector</p>
-	<button on:click={async () => await connection(1)}>connect</button>
+		<p>Connect With InjectedConnector</p>
+		<button on:click={async () => await connection(1)}>connect</button>
+	{/if}
+{:else}
+	<h1>Svelte Wagmi Not Configured</h1>
 {/if}
